79c7c1ab29e10f6d84704e31c5812656cb799cba
musa-alamgeer <72962670+musa-alamgeer@users.noreply.github.com>
2020-11-13 10:30:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <h1>Friday, 13 November 2020</h1>
+        </div
         <p>
           Musa Alamgeer 2017352 - Assignment 4
         </p>
